cfdfe6c253a049becb2ab19cdf93159007b24de5
Alexeygmr <alexey@cravter.com>
2022-04-27 10:40:42 +0300

    add Features section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ReactComponent as BitcoinChart} from "../../assets/ChartBitcoin.svg";
 import {ReactComponent as StaticChart} from "../../assets/StatisticChart.svg";
+import {ReactComponent as TableChart} from "../../assets/TableIcon.svg";
 import styles from './Features.module.scss'
 
 
@@ -23,8 +24,8 @@ const Features = (props) => {
 
        </div>
        <div className={styles.featuresStatisticInfoContainer}>
-         <StaticChart/>
-         <div className={styles.featuresBitcoinInfoTextContainer}>
+         <StaticChart className={styles.featuresStatisticInfoImage}/>
+         <div className={styles.featuresBitcoinStatisticTextContainer}>
            <h3 className={styles.featuresBitcoinTitle}>Detailed Statistics</h3>
            <span className={styles.featuresBitcoinSecondaryText}>View all mining related information in realtime, at any point at any location and decide which polls you want to mine in. </span>
            <button className={styles.featuresBitcoinLearnMoreButton}>
@@ -33,6 +34,18 @@ const Features = (props) => {
 
          </div>
 
+
+       </div>
+       <div className={styles.featuresBitcoinInfoContainer}>
+         <div className={styles.featuresBitcoinInfoTableTextContainer}>
+           <h3 className={styles.featuresBitcoinTitle}>Grow your profit and track your investments</h3>
+           <span className={styles.featuresBitcoinSecondaryText}>Get full statistic information about the behaviour of buyers and sellers will help you to make the decision. </span>
+           <button className={styles.featuresBitcoinLearnMoreButton}>
+             Learn More
+           </button>
+
+         </div>
+         <TableChart className={styles.featuresTableIcon}/>
 
        </div>
 
