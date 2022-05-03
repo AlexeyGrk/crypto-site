4d9c3d4c3048e8be9369efcd0704388c985b23ae
Alexeygmr <alexey@cravter.com>
2022-05-03 16:25:12 +0300

    bugfix burger menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Header from "./components/Header/Header";
 import WhyWeComponent from "./components/WhyContainer/WhyContainer";
 import TradeSecurely from "./components/TradeSecuraly/TradeSecurely";
